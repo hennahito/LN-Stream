9f6d1f04a2ef5d97478839afcf4fa1d363c4ebe9
hennahito <102311020+hennahito@users.noreply.github.com>
2024-03-04 22:05:37 +0900

    花粉症つらいです。

--- index.php
+++ index.php
@@ -6,8 +6,17 @@ $mode=$_GET['mode'];
 //ストリーム生成モード
 if($mode=="stream"){
     $reruas_addr=api_address.'get/?query='.$id;
-    shell_exec("sh ./enc.sh ".$reruas_addr." ".$id);
-    $pid=trim($output);
+    //"enc.sh"を実行してffmpegでエンコードする。ffmpegの標準出力は"/dev/null(俗に言うnullデバイス)"に出力させる
+    shell_exec("sh ./enc.sh ".$reruas_addr." ".$id.'> /dev/null &');
+    //ストリームが再生可能か調べる
+    while(true){
+        if(file_exists("data/".$id."/stream.mpd")){
+            sleep(1);
+            echo '{"status":"ok","stream","'.$id.'"}';
+            break;
+        }
+    }
+    
 }
 //ストリーム破棄モード
 else if($mode=="kill"){
